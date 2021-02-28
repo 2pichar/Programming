ad52855d7419fa9bee043675cd1b5bb276882a87
Charles Ludden <charles.ludden@gmail.com>
2021-02-28 00:30:59 +0000

    started new project

--- main.cpp
+++ main.cpp
@@ -4,12 +4,10 @@ using namespace std;
 using namespace timer;
 
 int main(){
-	auto callback = [&](){cout << "Your Timer Has Gone Off!" << endl;};
+	//auto callback = [&](){cout << "Your Timer Has Gone Off!" << endl;};
 	/*map <string, Timeout> timers;
 	timers.emplace(to_string(100), setTimeout(callback, 1000));
 	cout << timers.size() << endl;
 	while(!(timers["100"].getCleared()){}*/
-	std::any test = 100;
-	test = 23;
-	test = 1.34f;
+	
 }
